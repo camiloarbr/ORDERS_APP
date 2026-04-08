cf93cc93f5aece713773904b3709006a66c59870
Camilo <Carbelaezr@eafit.edu.co>
2026-04-08 09:21:13 -0500

    Replace JsonPlaceholderUserRepository with FakeUserRepository for user email retrieval

--- orders_app/user_repository.py
+++ orders_app/user_repository.py
@@ -1,12 +1,7 @@
-import requests
-
 class JsonPlaceholderUserRepository:
     def get_user_email(self, user_id):
-        response = requests.get(
-            f'https://jsonplaceholder.typicode.com/users/{user_id}', timeout=5
-        )
-        response.raise_for_status()
-        return response.json()['email']
+        raise Exception("User service unavailable")
+
 
 class FakeUserRepository:
     def get_user_email(self, user_id):
